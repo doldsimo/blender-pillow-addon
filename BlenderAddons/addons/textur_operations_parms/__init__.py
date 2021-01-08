0839942455abf67775f6666868942d23a3ef37b7
doldsimo <simon.dold@hs-furtwangen.de>
2021-01-08 11:33:32 +0100

    TASK: adding file Browser

--- BlenderAddons/addons/textur_operations_parms/__init__.py
+++ BlenderAddons/addons/textur_operations_parms/__init__.py
@@ -20,10 +20,26 @@ bl_info = {
 ptvsd.enable_attach()
 
 
-class TexturOperationsParms(bpy.types.Operator):
+class TexturOperationsParms(bpy.types.Operator, bpy.types.StringProperty):
+
     bl_idname = "textur.operations_parms"
     bl_label = "Change image with operations"
     bl_options = {'REGISTER', 'UNDO'}
+
+    root_folder = bpy.props.StringProperty(
+        name="File path",
+        description="Some elaborate description",
+        default="",
+        maxlen=1024,
+        subtype="FILE_PATH"
+    )
+
+    new_image_name = bpy.props.StringProperty(
+        name="File name",
+        description="Some elaborate description",
+        default="",
+        maxlen=1024
+    )
 
     solarize_threshold = bpy.props.IntProperty(
         name="Solarize threshold",
@@ -54,35 +70,49 @@ class TexturOperationsParms(bpy.types.Operator):
     )
 
     def execute(self, context):
-        im = Image.open("C:\\Users\\simon\\Desktop\\textur\\textur.jpg")
-        im = ImageOps.solarize(im, self.solarize_threshold)
-        if(self.greyscale):
-            im = ImageOps.grayscale(im)
-        if(self.invert):
-            im = ImageOps.invert(im)
-        im = im.rotate(self.rotate)
-        im.save("C:\\Users\\simon\\Desktop\\textur\\textur_edited.jpg")
 
-        mat = bpy.data.materials.new(name="New_Mat")
-        mat.use_nodes = True
-        bsdf = mat.node_tree.nodes["Principled BSDF"]
-        texImage = mat.node_tree.nodes.new('ShaderNodeTexImage')
-        texImage.image = bpy.data.images.load(
-            "C:\\Users\\simon\\Desktop\\textur\\textur_edited.jpg")
-        mat.node_tree.links.new(
-            bsdf.inputs['Base Color'], texImage.outputs['Color'])
-        ob = context.view_layer.objects.active
+        if(self.root_folder):
+            imList = self.root_folder.rsplit("\\", 1)
 
-        # Assign it to object
-        ob.data.materials[0] = mat
+            imPath = imList[0]
+            fileOldName = imList[1]
+
+            fileType = fileOldName.rsplit(".", 1)
+
+            if(self.new_image_name):
+                newPath = imPath + "\\" + self.new_image_name + "." + fileType[1]
+                
+                im = Image.open(self.root_folder)
+                print(self.root_folder)
+                im = ImageOps.solarize(im, self.solarize_threshold)
+                if(self.greyscale):
+                    im = ImageOps.grayscale(im)
+                if(self.invert):
+                    im = ImageOps.invert(im)
+                im = im.rotate(self.rotate)
+                im.save(newPath)
+
+                mat = bpy.data.materials.new(name="New_Mat")
+                mat.use_nodes = True
+                bsdf = mat.node_tree.nodes["Principled BSDF"]
+                texImage = mat.node_tree.nodes.new('ShaderNodeTexImage')
+                texImage.image = bpy.data.images.load(newPath)
+                mat.node_tree.links.new(
+                    bsdf.inputs['Base Color'], texImage.outputs['Color'])
+                ob = context.view_layer.objects.active
+
+                # Assign it to object
+                ob.data.materials[0] = mat
         return {'FINISHED'}
 
 
 def register():
     print("Registering Textur Operations Parms")
     bpy.utils.register_class(TexturOperationsParms)
+    # bpy.utils.register_class(OHA_QuickLink_Props)
 
 
 def unregister():
     print("Unregistering Textur Operations Parms")
     bpy.utils.unregister_class(TexturOperationsParms)
+    # bpy.utils.unregister_class(OHA_QuickLink_Props)
