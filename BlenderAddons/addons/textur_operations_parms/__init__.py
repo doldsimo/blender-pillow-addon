97eb02896769fcf73299772e7c22597da705c72c
doldsimo <simon.dold@hs-furtwangen.de>
2021-01-15 18:02:17 +0100

    TASK: update init

--- BlenderAddons/addons/textur_operations_parms/__init__.py
+++ BlenderAddons/addons/textur_operations_parms/__init__.py
@@ -16,6 +16,8 @@ from bpy.props import (StringProperty,
 
 from PIL import Image
 from PIL import ImageOps
+import cv2 as cv
+import numpy as np
 
 from . import sepia
 
@@ -155,7 +157,7 @@ class Image_Panel(Panel):
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     bl_category = "Image Editing"
-    bl_context = "objectmode"
+    # bl_context = "objectmode"
 
     @ classmethod
     def poll(self, context):
@@ -176,7 +178,7 @@ class Image_Correction_Panel(Panel):
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     bl_category = "Image Editing"
-    bl_context = "objectmode"
+    # bl_context = "objectmode"
 
     @ classmethod
     def poll(self, context):
@@ -198,7 +200,7 @@ class Filter_Panel(Panel):
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     bl_category = "Image Editing"
-    bl_context = "objectmode"
+    # bl_context = "objectmode"
 
     @ classmethod
     def poll(self, context):
@@ -222,7 +224,7 @@ class Transformations_Panel(Panel):
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     bl_category = "Image Editing"
-    bl_context = "objectmode"
+    # bl_context = "objectmode"
 
     @ classmethod
     def poll(self, context):
@@ -238,9 +240,56 @@ class Transformations_Panel(Panel):
         layout.prop(mytool, "flip_horizontally")
         layout.prop(mytool, "scale_image")
 
+class Magic_Wand_Panel(Panel):
+    bl_label = "Magic Wand"
+    bl_idname = "OBJECT_PT_custom_panel_Magic_Wand"
+    bl_space_type = "IMAGE_EDITOR"
+    bl_region_type = "UI"
+    bl_category = "Image Editing"
+    # bl_context = "objectmode"
+
+    @ classmethod
+    def poll(self, context):
+        return context.object is not None
+
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+        mytool = scene.my_tool
+
+        if(mytool.root_folder and mytool.new_image_name):
+            layout.operator("textur.magicwand")
+
+
 # ------------------------------------------------------------------------
 #    Operators
 # ------------------------------------------------------------------------
+class MagicWand(Operator):
+    bl_label = "Open Magic Wand"
+    bl_idname = "textur.magicwand"
+
+    def execute(self, context):
+        scene = context.scene
+        mytool = scene.my_tool
+        print("open Magic Wand")
+
+        if(mytool.root_folder):
+            imList = mytool.root_folder.rsplit("\\", 1)
+
+            imPath = imList[0]
+            fileOldName = imList[1]
+
+            fileType = fileOldName.rsplit(".", 1)
+
+            if(mytool.new_image_name):
+                newPath = imPath + "\\" + \
+                    mytool.new_image_name + "." + fileType[1]
+
+                img = cv.imread(newPath)
+                window = SelectionWindow(img)
+                window.show()
+
+        return {'FINISHED'}
 
 
 class WM_OT_HelloWorld(Operator):
@@ -252,10 +301,10 @@ class WM_OT_HelloWorld(Operator):
         mytool = scene.my_tool
 
         # print the values to the console
-        print("Data:")
-        print("Path:", mytool.root_folder)
-        print("New Image Name:", mytool.new_image_name)
-        print("Boolean_2:", "dfadsf")
+        # print("Data:")
+        # print("Path:", mytool.root_folder)
+        # print("New Image Name:", mytool.new_image_name)
+        # print("Boolean_2:", "dfadsf")
 
         if(mytool.root_folder):
             imList = mytool.root_folder.rsplit("\\", 1)
@@ -293,24 +342,24 @@ class WM_OT_HelloWorld(Operator):
                 im.save(newPath)
 
                 ob = context.view_layer.objects.active
-                if(ob.active_material is None):
-                    print("New Material")
-                    mat = bpy.data.materials.new(name="New_Mat")
-                    mat.use_nodes = True
-                    bsdf = mat.node_tree.nodes["Principled BSDF"]
-                    texImage = mat.node_tree.nodes.new('ShaderNodeTexImage')
-                    texImage.image = bpy.data.images.load(newPath)
-                    mat.node_tree.links.new(
-                        bsdf.inputs['Base Color'], texImage.outputs['Color'])
-                else:
-                    print("Existing Material")
-                    mat = ob.active_material
-                    mat.use_nodes = True
-                    bsdf = mat.node_tree.nodes["Principled BSDF"]
-                    texImage = mat.node_tree.nodes.new('ShaderNodeTexImage')
-                    texImage.image = bpy.data.images.load(newPath)
-                    mat.node_tree.links.new(
-                        bsdf.inputs['Base Color'], texImage.outputs['Color'])
+                # if(ob.active_material is None):
+                print("New Material")
+                mat = bpy.data.materials.new(name="New_Mat")
+                mat.use_nodes = True
+                bsdf = mat.node_tree.nodes["Principled BSDF"]
+                texImage = mat.node_tree.nodes.new('ShaderNodeTexImage')
+                texImage.image = bpy.data.images.load(newPath)
+                mat.node_tree.links.new(
+                    bsdf.inputs['Base Color'], texImage.outputs['Color'])
+                # else:
+                #     print("Existing Material")
+                #     mat = ob.active_material
+                #     mat.use_nodes = True
+                #     bsdf = mat.node_tree.nodes["Principled BSDF"]
+                #     texImage = mat.node_tree.nodes.new('ShaderNodeTexImage')
+                #     texImage.image = bpy.data.images.load(newPath)
+                #     mat.node_tree.links.new(
+                #         bsdf.inputs['Base Color'], texImage.outputs['Color'])
 
                 # Assign it to object
                 ob.data.materials[0] = mat
@@ -322,13 +371,104 @@ class WM_OT_HelloWorld(Operator):
 # ------------------------------------------------------------------------
 
 
+SHIFT_KEY = cv.EVENT_FLAG_SHIFTKEY
+ALT_KEY = cv.EVENT_FLAG_ALTKEY
+
+
+def _find_exterior_contours(img):
+    ret = cv.findContours(img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    if len(ret) == 2:
+        return ret[0]
+    elif len(ret) == 3:
+        return ret[1]
+    raise Exception("Check the signature for `cv.findContours()`.")
+
+
+class SelectionWindow:
+    def __init__(self, img, name="Magic Wand Selector", connectivity=4, tolerance=32):
+        self.name = name
+        h, w = img.shape[:2]
+        self.img = img
+        self.mask = np.zeros((h, w), dtype=np.uint8)
+        self._flood_mask = np.zeros((h + 2, w + 2), dtype=np.uint8)
+        self._flood_fill_flags = (
+            connectivity | cv.FLOODFILL_FIXED_RANGE | cv.FLOODFILL_MASK_ONLY | 255 << 8
+        )  # 255 << 8 tells to fill with the value 255
+        cv.namedWindow(self.name)
+        self.tolerance = (tolerance,) * 3
+        cv.createTrackbar(
+            "Tolerance", self.name, tolerance, 255, self._trackbar_callback
+        )
+        cv.setMouseCallback(self.name, self._mouse_callback)
+
+    def _trackbar_callback(self, pos):
+        self.tolerance = (pos,) * 3
+
+    def _mouse_callback(self, event, x, y, flags, *userdata):
+
+        if event != cv.EVENT_LBUTTONDOWN:
+            return
+
+        modifier = flags & (ALT_KEY + SHIFT_KEY)
+
+        self._flood_mask[:] = 0
+        cv.floodFill(
+            self.img,
+            self._flood_mask,
+            (x, y),
+            0,
+            self.tolerance,
+            self.tolerance,
+            self._flood_fill_flags,
+        )
+        flood_mask = self._flood_mask[1:-1, 1:-1].copy()
+
+        if modifier == (ALT_KEY + SHIFT_KEY):
+            self.mask = cv.bitwise_and(self.mask, flood_mask)
+        elif modifier == SHIFT_KEY:
+            self.mask = cv.bitwise_or(self.mask, flood_mask)
+        elif modifier == ALT_KEY:
+            self.mask = cv.bitwise_and(self.mask, cv.bitwise_not(flood_mask))
+        else:
+            self.mask = flood_mask
+
+        self._update()
+
+    def _update(self):
+        """Updates an image in the already drawn window."""
+        viz = self.img.copy()
+        contours = _find_exterior_contours(self.mask)
+        viz = cv.drawContours(
+            viz, contours, -1, color=(255,) * 3, thickness=-1)
+        viz = cv.addWeighted(self.img, 0.75, viz, 0.25, 0)
+        viz = cv.drawContours(viz, contours, -1, color=(255,) * 3, thickness=1)
+
+        self.mean, self.stddev = cv.meanStdDev(self.img, mask=self.mask)
+        meanstr = "mean=({:.2f}, {:.2f}, {:.2f})".format(*self.mean[:, 0])
+        stdstr = "std=({:.2f}, {:.2f}, {:.2f})".format(*self.stddev[:, 0])
+        cv.imshow(self.name, viz)
+        cv.displayStatusBar(self.name, ", ".join((meanstr, stdstr)))
+
+    def show(self):
+        """Draws a window with the supplied image."""
+        self._update()
+        print("Press [q] or [esc] to close the window.")
+        while True:
+            k = cv.waitKey() & 0xFF
+            if k in (ord("q"), ord("\x1b")):
+                cv.destroyWindow(self.name)
+                break
+
+
 classes = (
     AllProperties,
     WM_OT_HelloWorld,
+    MagicWand,
     Image_Panel,
     Image_Correction_Panel,
     Filter_Panel,
-    Transformations_Panel
+    Transformations_Panel,
+    Magic_Wand_Panel
 )
 
 
