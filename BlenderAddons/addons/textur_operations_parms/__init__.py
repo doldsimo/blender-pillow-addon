08917d8a9dd937ca7978735d4f1a2f50652d86f4
doldsimo <simon.dold@hs-furtwangen.de>
2021-01-17 21:35:11 +0100

    Update __init__.py

--- BlenderAddons/addons/textur_operations_parms/__init__.py
+++ BlenderAddons/addons/textur_operations_parms/__init__.py
@@ -1,43 +1,48 @@
 import bpy
 import ptvsd
 
+
 from PIL import Image
 from PIL import ImageOps
 
 from . import sepia
-from magicwand import SelectionWindow
-import cv2 as cv
-
-import tkinter as tk
-import tkinter.ttk as ttk
-from tkcolorpicker import askcolor
 
 bl_info = {
     "name": "Textur operations parms",
     "author": "Simon Dold",
     "version": (1, 0, 0),
-    "blender": (2, 91, 0),
+    "blender": (2, 90, 0),
     "description": "Change image with operations",
     "category": "Mesh",
     "support": "TESTING",
     "location": "View 3D > Edit Mode > Mesh",
 }
- 
+
 
 ptvsd.enable_attach()
 
-    
 
-class TexturOperationsParms(bpy.types.Operator, bpy.types.PropertyGroup):
+
+class TexturOperationsParms(bpy.types.Operator, bpy.types.StringProperty):
+
     bl_idname = "textur.operations_parms"
     bl_label = "Change image with operations"
     bl_options = {'REGISTER', 'UNDO'}
 
-    root_folder : bpy.props.StringProperty(
-        name="Root Folder",
-        description="Only .blend files two levels below this folder will be listed.",
-        subtype="FILE_PATH",
-        )
+    root_folder = bpy.props.StringProperty(
+        name="File path",
+        description="Some elaborate description",
+        default="",
+        maxlen=1024,
+        subtype="FILE_PATH"
+    )
+
+    new_image_name = bpy.props.StringProperty(
+        name="File name",
+        description="Some elaborate description",
+        default="",
+        maxlen=1024
+    )
 
     solarize_threshold = bpy.props.IntProperty(
         name="Solarize threshold",
@@ -80,48 +85,59 @@ class TexturOperationsParms(bpy.types.Operator, bpy.types.PropertyGroup):
     )
 
     def execute(self, context):
-        im = Image.open("C:\\Users\\chris\\Desktop\\taylor-volek-space-orc-weak-male-04.jpg")
-        im = ImageOps.solarize(im, self.solarize_threshold)
-        if(self.greyscale):
-            im = ImageOps.grayscale(im)
-        if(self.invert):
-            im = ImageOps.invert(im)
-        im = im.rotate(self.rotate)
-        im.save("C:\\Users\\chris\\Desktop\\taylor-volek-space-orc-weak-male-04.jpg")
 
-        mat = bpy.data.materials.new(name="New_Mat")
-        mat.use_nodes = True
-        bsdf = mat.node_tree.nodes["Principled BSDF"]
-        texImage = mat.node_tree.nodes.new('ShaderNodeTexImage')
-        texImage.image = bpy.data.images.load(
-            "C:\\Users\\chris\\Desktop\\taylor-volek-space-orc-weak-male-04.jpg")
-        mat.node_tree.links.new(
-            bsdf.inputs['Base Color'], texImage.outputs['Color'])
-        ob = context.view_layer.objects.active
+        if(self.root_folder):
+            imList = self.root_folder.rsplit("\\", 1)
+
+            imPath = imList[0]
+            fileOldName = imList[1]
+
+            fileType = fileOldName.rsplit(".", 1)
+
+            if(self.new_image_name):
+                newPath = imPath + "\\" + self.new_image_name + "." + fileType[1]
+                
+                im = Image.open(self.root_folder)
+                print(self.root_folder)
+                im = ImageOps.solarize(im, self.solarize_threshold)
+                if(self.greyscale):
+                    im = ImageOps.grayscale(im)
+                if(self.invert):
+                    im = ImageOps.invert(im)
+                if(self.sepia):
+                    im = sepia.convert_sepia(im)
+                if(self.blackAndWhite):
+                    thresh = 100
+                    fn = lambda x : 255 if x > thresh else 0
+                    im = im.convert('L').point(fn, mode='1')
+
+                im = im.rotate(self.rotate)
+                im.save(newPath)
+
+                ob = context.view_layer.objects.active
+                if(ob.active_material is None):
+                    print("New Material")
+                    mat = bpy.data.materials.new(name="New_Mat")
+                    mat.use_nodes = True
+                    bsdf = mat.node_tree.nodes["Principled BSDF"]
+                    texImage = mat.node_tree.nodes.new('ShaderNodeTexImage')
+                    texImage.image = bpy.data.images.load(newPath)
+                    mat.node_tree.links.new(
+                        bsdf.inputs['Base Color'], texImage.outputs['Color'])
+                else:
+                    print("Existing Material")
+                    mat = ob.active_material
+                    mat.use_nodes = True
+                    bsdf = mat.node_tree.nodes["Principled BSDF"]
+                    texImage = mat.node_tree.nodes.new('ShaderNodeTexImage')
+                    texImage.image = bpy.data.images.load(newPath)
+                    mat.node_tree.links.new(
+                        bsdf.inputs['Base Color'], texImage.outputs['Color'])
 
                 # Assign it to object
-        ob.data.materials[0] = mat
+                ob.data.materials[0] = mat
         return {'FINISHED'}
 
-
-    
-    #Zauberstab
-    #img = cv.imread("C:\\Users\\chris\\Desktop\\taylor-volek-space-orc-weak-male-04.jpg")
-    #window = SelectionWindow(img)
-    #window.show()
-
-#Pipette
-root = Tk()
-root.title("Pipette")
-root.geometry("400x400")
-
-def color():
-    my_color = colorchooser.askcolor()[0]
-    my_label = Label(root, text=my_color).pack(pady=10)
-
-my_button = Button(root, text="pick a Color", command=color).pack()
-
-root.mainloop
 
 def register():
     print("Registering Textur Operations Parms")
